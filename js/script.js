97204ca846fcaba95f45934f1709dc8f2946b4fc
Kamil Makoś <makos.kamil@o2.pl>
2018-04-04 23:43:31 +0200

    init

---
+++ js/script.js
@@ -0,0 +1,91 @@
+$(function() {
+
+    function randomString(){
+        var chars = '0123456789abcdefghijklmnopqrstuvwxyzABCDERGHIJKLMNOPQRSTUVWXYZ';
+
+        var str = '';
+
+        for (var i = 0; i <10; i++) {
+            str += chars[Math.floor(Math.random() * chars.legth)];
+        }
+        return str;
+        //czy tu powinna być jeszcze funkcja sprawdzająca czy istnieje duplikat? bo rozumiem, że nazwa przypisywana jest losowo za każdym razem?
+    }
+    function Column(name) {
+
+        var self = this;
+
+        this.id = randomString();
+        this.name = name;
+        this.$element = createColumn();
+
+        function.createColumn() {
+
+            var $column = $('<div>').addClass('column');
+            var $columnTitle = $('<h2>').addClass('column-title').text(self.name);
+            var $columnCardList = $('<ul>').addClass('column-card-list');
+            var $columnDelete = $('<button>').addClass('btn-delete').text('x');
+            var $columnAddCard = $('<button>').addClass('add-card').text('Add a card');
+
+            $columnDelete.click(function() {
+
+                self.removeColumn();
+            });
+
+            $columnAddCard.click(function() {
+
+                self.addCard(new Card(prompt("Enter the name of the card")));
+            });
+
+            $column.append($columnTitle)
+                    .append($columnDelete)
+                    .append($columnAddCard)
+                    .append($columnCardList);
+            return $column;
+        }
+
+        Column.prototype = {
+            
+            addCard: function(card) {
+                this.$element.children('ul').append(card.$element);
+            },
+
+            removeColumn: function() {
+                this.$element.remove();
+            }
+        };
+    }
+    function Card(description) {
+
+        var self = this;
+
+        this.id = randomString();
+        this.description = description;
+        this.$element = creatCard();
+
+        function createCard() {
+
+            var $card = $('<li>').addClass('card');
+            var $cardDescription = $('<p>').addClass('card-description').text(self.description);
+            var $cardDelete = $('<button>').addClass('btn-delete').text('x');
+
+            $cardDelete.click(function(){
+
+                self.removeCard();
+            });
+
+            $card.append($cardDelete)
+
+                .append($cardDescription);
+
+            return $card;    
+            }
+        Card.prototype = {
+
+            removeCard: function() {
+                this.$element.remove();
+            }
+        }
+        }
+    }
+})
